694308a5ac47387059acdcf795d437d4c5a8ec14
Udbhaw Anuj <udbhawanuj53@gmail.com>
2026-01-17 23:42:39 +0530

    Add files via upload

---
+++ MaxDepthTree.java
@@ -0,0 +1,29 @@
+public class MaxDepthTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+            left = right = null;
+        }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(
+                maxDepth(root.left),
+                maxDepth(root.right)
+        );
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+
+        System.out.println(maxDepth(root));
+    }
+}
