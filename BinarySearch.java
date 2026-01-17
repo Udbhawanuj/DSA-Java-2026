e18355a9ea1401438851075c4a836f7c4316b7a9
Udbhaw Anuj <udbhawanuj53@gmail.com>
2026-01-17 23:39:40 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,23 @@
+public class BinarySearch {
+    public static int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 7, 9};
+        int target = 7;
+
+        System.out.println(search(nums, target));
+    }
+}
